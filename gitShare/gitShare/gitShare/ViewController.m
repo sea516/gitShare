14feefe3f7387828d7f8c3b1b8ed4ec71fd2ca47
Haiyang Jiang <jianghaiyang2010@126.com>
2015-08-20 12:55:24 +0800

    Revert "second commit"
    This reverts commit 654daf2fc0dac09b6cc46e7e59d83dd8eb0acffe.

--- gitShare/gitShare/gitShare/ViewController.m
+++ gitShare/gitShare/gitShare/ViewController.m
@@ -18,7 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     // commit first
-    // commit second
 }
 
 - (void)didReceiveMemoryWarning {
