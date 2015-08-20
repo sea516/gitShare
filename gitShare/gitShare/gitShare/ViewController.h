166f36353a6635d57b98c6581b29948c628055e5
Haiyang Jiang <jianghaiyang2010@126.com>
2015-08-20 08:56:59 +0800

    Initial Commit

---
+++ gitShare/gitShare/gitShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitShare
+//
+//  Created by Haiyang Jiang on 15/8/20.
+//  Copyright (c) 2015年 Haiyang Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitShare/gitShare/gitShare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitShare
+//
+//  Created by Haiyang Jiang on 15/8/20.
+//  Copyright (c) 2015年 Haiyang Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
